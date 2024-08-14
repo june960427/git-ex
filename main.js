f28e8c923d24f79ea3b1372bf1188f938c493422
june960427 <ljh975896@gmail.com>
2024-08-14 15:13:37 +0900

    로그인 기능 완성. 2번 째 줄 내용.. 3번 째 줄 내용...

--- main.js
+++ main.js
@@ -7,5 +7,10 @@
  * git config --global init.defaultbranch main
  * => 초기화 할 때 마스터 말고 main으로 지정
  *
+ * git branch login => branch 생성
+ * git branch => branch 종류 확인 가능
+ * branch 이동할 때는 checkout말고 switch쓰는게 좋음
+ * branch 지울 때 git branch -D login
  *
+ * 만들면서 branch 이동할 때 git switch -c login
  */
